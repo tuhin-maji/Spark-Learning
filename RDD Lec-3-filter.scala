e91582aa5b826ccaff4ce1fa6fc72a45db138a2a
tuhintomas <55351623+tuhintomas@users.noreply.github.com>
2025-01-23 19:04:04 +0530

    Add files via upload

---
+++ RDD Lec-3-filter.scala
@@ -0,0 +1,14 @@
+// Databricks notebook source
+//RDD Creation
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder.appName("CreateRDD").getOrCreate()
+val data = Seq(1, 2, 3, 4, 5)
+val rdd1 = spark.sparkContext.parallelize(data)
+//filter 
+//Filter only even numbers
+val filtered_rdd = rdd1.filter(x=>x%2==0)
+filtered_rdd.collect().foreach(println)
+
+// COMMAND ----------
+
+
